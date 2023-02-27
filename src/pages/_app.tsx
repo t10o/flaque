eb4b04fd3421d2f84ea2636027c3c8b4eaa49f16
t10o <dev@t10o.one>
2023-02-27 21:52:18 +0900

    :heavy_plus_sign: react-toastifyを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Noto_Sans_Javanese } from "next/font/google";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
 
 import { Layout } from "@/components/layouts";
@@ -23,7 +25,20 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Flaque</title>
       </Head>
-      
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
       <ApolloProvider client={client}>
         <style jsx global>{`
           html {
