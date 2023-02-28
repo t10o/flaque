fdb60d6dd16d5e13cdf343c5fd54ca4faea8dba2
t10o <dev@t10o.one>
2023-03-01 00:38:50 +0900

    :rocket: パフォーマンス改善？

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,15 +2,11 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 export const initializeApollo = () => {
   const link = createHttpLink({
-    fetch,
     uri: process.env.NEXT_PUBLIC_ENDPOINT,
   });
 
-  const client = new ApolloClient({
+  return new ApolloClient({
     cache: new InMemoryCache(),
     link,
-    ssrMode: true,
   });
-
-  return client;
 };
