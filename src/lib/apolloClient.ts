d96ca431faa70f3fac60735af4ccfa6a730ad525
t10o <dev@t10o.one>
2023-03-01 00:44:33 +0900

    :bug: ロールバック

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,11 +2,13 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 export const initializeApollo = () => {
   const link = createHttpLink({
+    fetch,
     uri: process.env.NEXT_PUBLIC_ENDPOINT,
   });
 
   return new ApolloClient({
     cache: new InMemoryCache(),
     link,
+    ssrMode: true,
   });
 };
