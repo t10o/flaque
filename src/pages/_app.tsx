624912af1fee774f394af4f954641470e1c2a0b7
t10o <dev@t10o.one>
2023-03-04 18:38:34 +0900

    :+1: 不要な処理を削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loading-skeleton/dist/skeleton.css";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Noto_Sans_Javanese } from "next/font/google";
@@ -18,17 +17,13 @@ import * as gtag from "@/lib/gtag";
 const notoSansJapanese = Noto_Sans_Javanese({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: process.env.NEXT_PUBLIC_ENDPOINT,
-  });
-
   return (
     <>
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
       />
+
       <Script
         id="gtag-init"
         strategy="afterInteractive"
@@ -37,7 +32,6 @@ export default function App({ Component, pageProps }: AppProps) {
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
- 
            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
            `,
         }}
@@ -72,17 +66,15 @@ export default function App({ Component, pageProps }: AppProps) {
         theme="light"
       />
 
-      <ApolloProvider client={client}>
-        <style jsx global>{`
-          html {
-            font-family: ${notoSansJapanese.style.fontFamily};
-          }
-        `}</style>
+      <style jsx global>{`
+        html {
+          font-family: ${notoSansJapanese.style.fontFamily};
+        }
+      `}</style>
 
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ApolloProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
 
       <Analytics />
     </>
