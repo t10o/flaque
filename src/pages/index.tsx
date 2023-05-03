6cc1ae8f91de30386b66ab003034137fa186f426
t10o <dev@t10o.one>
2023-05-03 14:34:02 +0900

    :+1: ページの整理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { GetStaticProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
 import { Profile } from "src/features/profile";
 
 import { ContentLayout } from "@/components/layouts";
 
 export const Home: NextPage = () => {
   return (
-    <ContentLayout>
-      <Profile />
-    </ContentLayout>
+    <>
+      <NextSeo
+        title="Home | flaque"
+        description="flaqueの運営のプロフィール"
+        openGraph={{
+          title: "Home | flaque",
+          description: "flaqueの運営のプロフィール",
+          url: "https://flaque.t10o.one",
+        }}
+      />
+
+      <ContentLayout>
+        <Profile />
+      </ContentLayout>
+    </>
   );
 };
 
